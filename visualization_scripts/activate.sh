050d927454f4d48ba04e4265088d4e3b9583ddba
Anthony Gitter <gitter@biostat.wisc.edu>
2020-03-20 16:47:27 -0500

    Rename conda environment

--- visualization_scripts/activate.sh
+++ visualization_scripts/activate.sh
@@ -3,13 +3,13 @@
 #conda init bash
 echo "----------------------------starting script----------------------------"
 eval "$(conda shell.bash hook)"
-conda activate py2cyto_env
+conda activate py2cyto
 
 #echo "check enviornment"
 if [ $? -ne 0 ]; then
 	echo "-creating py2cyto environment"
 	conda env create -f minimal_env.yml
-	conda activate py2cyto_env
+	conda activate py2cyto
 fi
 echo "-environment activated"
 
