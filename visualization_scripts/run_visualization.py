84d783a26e664b05cb8947175804026eeaead923
Adam Shedivy <ajshedivy@wisc.edu>
2020-05-13 20:25:40 -0500

    run egfr utilities

--- visualization_scripts/run_visualization.py
+++ visualization_scripts/run_visualization.py
@@ -5,17 +5,18 @@ script loads style file and TPS output file into Cytoscape session
 
 """
 
-#imports
+# imports
 import sys
 import time
 import subprocess
 import os
 from requests.exceptions import ConnectionError as CE
-from json.decoder import JSONDecodeError 
+from json.decoder import JSONDecodeError
 from py2cytoscape.data.cyrest_client import CyRestClient
 from py2cytoscape import cyrest
 
-#assert Cytoscape is running on machine
+# assert Cytoscape is running on machine
+
 
 def process_exists(process_name):
     """
@@ -56,7 +57,7 @@ def find_path(name, path):
     # list to hold found matches
     result = []
 
-    # walk directory/file tree 
+    # walk directory/file tree
     for root, dirs, files in os.walk(path):
         if name in files:
             result.append(os.path.join(root, name))
@@ -69,10 +70,10 @@ def vis(output, style):
         output: output file from tps software
         style: style file for cytoscpe 
     """
-    
-    #create cyrest client
+
+    # create cyrest client
     cyy = cyrest.cyclient()
-    
+
     # Step 1: Create py2cytoscape client
     cy = CyRestClient()
 
@@ -86,17 +87,15 @@ def vis(output, style):
     print("---Done")
     time.sleep(2)
 
-
     # Step 3: Apply layout
     # list of styles to apply
-    # only contains the one style loaded from style file 
+    # only contains the one style loaded from style file
     print("---apply style file")
     style = cyy.vizmap.load_file(style)
     print("---style name: ", style)
     cyy.vizmap.apply(style[0])
 
     print("Done! open up cytoscape session")
-
 
 
 def main(args):
@@ -108,33 +107,30 @@ def main(args):
     CYTOSCAPE = 'Cytoscape.exe'
 
     if OUTPUT_FILE.lower().endswith(".sif"):
-        print ("file has valid extention")
+        print("file has valid extention")
     else:
-        print ("invalid extension on output file: ", OUTPUT_FILE)
+        print("invalid extension on output file: ", OUTPUT_FILE)
         sys.exit()
 
     # get absolute paths if nessesay
     OUTPUT_FILE = os.path.abspath(OUTPUT_FILE)
     STYLE_FILE = os.path.abspath(STYLE_FILE)
-    Cytoscape = os.path.abspath("Cytoscape.exe");
+    Cytoscape = os.path.abspath("Cytoscape.exe")
     print("")
     print("---absolute output path: ", OUTPUT_FILE)
-    print("---absolute style path: ", STYLE_FILE);
+    print("---absolute style path: ", STYLE_FILE)
 
-    
-    #!TODO check style file extensiona and format 
-
-
-    # check if Cytoscape is running 
+    #!TODO check style file extensiona and format
+    # check if Cytoscape is running
     is_running = process_exists('Cytoscape.exe')
 
     if (is_running == False):
         print("---Cytoscape not running")
 
-        # find path to Cytoscape on machine 
+        # find path to Cytoscape on machine
         cyto_path = find_path(CYTOSCAPE, DIRNAME)
 
-        # open Cytoscape 
+        # open Cytoscape
         p = subprocess.Popen(cyto_path)
 
     else:
@@ -144,14 +140,14 @@ def main(args):
 
     # call vis fuction to load input files in Cytoscape session
     # catch errors: ConnectionError, JSONDecoderError
-    connection_count = 0 
+    connection_count = 0
     JSON_count = 0
     switch = False
     start = time.time()
     while switch is False:
         try:
 
-            # run visualization function 
+            # run visualization function
             vis(OUTPUT_FILE, STYLE_FILE)
 
             # print information --------------------------------
@@ -166,8 +162,8 @@ def main(args):
             switch = True
 
         except CE as e:
-            
-            #requests.ConnectionError caught if connection cannot be made with Cytoscape Server
+
+            # requests.ConnectionError caught if connection cannot be made with Cytoscape Server
             connection_count += 1
             time.sleep(2)
             switch = False
@@ -181,10 +177,9 @@ def main(args):
             switch = False
             continue
     end = time.time()
-    print("---time elapsed: ", end - start)  
+    print("---time elapsed: ", end - start)
 
     print("---Finish loading output file and style file")
-  
 
 
 if __name__ == '__main__':
